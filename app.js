16f24b1fd4d7b63fd42871fc3193ff27d8b7b17e
samihashehatta <samihashehatta@gmail.com>
2019-02-04 18:20:41 +0200

    Making changeing 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =require('express');
 const mongoose =require('mongoose');
 const app = express();
 const databaseURL= 'mongodb://samiha:SAM3000sam@ds239703.mlab.com:39703/storyteller' || 'mongodb://localhost/storybook';
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 
