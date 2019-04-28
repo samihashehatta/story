06b4996b38017f8bb4dd67f410cffde47a0a0cee
samihashehatta <samihashehatta@gmail.com>
2019-04-28 20:19:33 +0200

    testing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const mongoose =require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const authRoutes =  require('./routes/auth');
 const passport =require('passport');
 const passportConfig = require('./config/passport');
